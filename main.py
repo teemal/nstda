da8725bdbbbc9b997b18db1526eaebf62ca1b51e
teemal <taymal1987@gmail.com>
2020-02-23 20:04:02 -0800

    finished closeness descriptions

--- main.py
+++ main.py
@@ -19,8 +19,13 @@ for ts, buf in pcap:
     #src and dst ip addresses
     src_ip= socket.inet_ntoa(ip.src)
     dst_ip= socket.inet_ntoa(ip.dst)
+
+    # get src and dst ports
+    tcp =  ip.data
+    src_port = tcp.sport
+    dst_port = tcp.dport
     #add edges to graph based on address
-    g.add_edge(src_ip, dst_ip)
+    g.add_edge(src_ip, dst_ip, sport=src_port, dport=dst_port)
 """
 Centrality is a very important concept in identifying important nodes in a graph. 
 It is used to measure the importance or centrality as in how central a node is in the graph) of various nodes in a graph. 
@@ -30,6 +35,18 @@ different perspective and further provides relevant analytical information about
 """
 centrality = {}
 
+"""
+This metric defines and measures the importance of a node in a network 
+based upon how many times it occurs in the shortest path between all pairs of nodes in a graph.
+
+For network
+    A
+    |
+D---B---C
+    |
+    K
+Node B  would have the highest betweeness since it lays between the shortest paths of  all other nodes
+"""
 print("Betweenness")
 b = nx.betweenness_centrality(g)
 for v in g.nodes():
@@ -39,16 +56,30 @@ In a non-directed graph, degree of a node is defined as the number of direct con
 In a directed graph (each edge has a direction), degree of a node is further divided into In-degree and Out-degree. 
 In-degree refers to the number of edges/connections incident on it 
 and Out-degree refers to the number of edges/connections from it to other nodes.
+
+In a network, degree represents how much one node speaks to others on the network
+If all communication went directly a central router, it  would have the highest degree
 """
 print("Degree centrality")
 d = nx.degree_centrality(g)
 for v in g.nodes():
     print("%s %f" % (v, d[v]))
 
+"""
+number of edges between two nodes
+
+If we have a network
+A-B-C-D
+
+the closeness between A and D is 3 and 1 for B and C
+"""
 print("Closeness centrality")
 c = nx.closeness_centrality(g)
 for v in g.nodes():
     print("%s %f" % (v, c[v]))
+
+for i in  c:
+    print(i)
 
 # nx.draw(g, with_labels = True) 
 # plt.savefig("filename.png")
