b2731cb9903c4e073e4a24348366e64a3efad187
teemal <taymal1987@gmail.com>
2020-02-24 22:29:36 -0800

    added bokeh example

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 import sys, dpkt, socket
 import networkx as nx
 import matplotlib.pyplot as plt
+from bokeh.io import show, output_file
+from bokeh.plotting import figure
+from bokeh.models.graphs import from_networkx
 
 #new graph
 g = nx.MultiGraph()
@@ -77,6 +80,17 @@ c = nx.closeness_centrality(g)
 for v in g.nodes():
     print("%s %f" % (v, c[v]))
 
+
+G=nx.karate_club_graph()
+
+plot = figure(title="Networkx Integration Demonstration", x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+              tools="", toolbar_location=None)
+
+graph = from_networkx(G, nx.spring_layout, scale=2, center=(0,0))
+plot.renderers.append(graph)
+
+output_file("networkx_graph.html")
+show(plot)
 # nx.draw(g, with_labels = True) 
 # plt.savefig("filename.png")
 
